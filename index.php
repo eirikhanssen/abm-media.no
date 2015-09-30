352dfc367e3fa7995fa63c319a12382656dc30c5
Eirik Hanssen <eirikhanssen79@gmail.com>
2015-09-30 11:44:50 +0200

    update news, add news archive

--- index.php
+++ index.php
@@ -16,64 +16,15 @@
 
 <body>
 <?php include 'inc_header.php'; ?>
-<?php include 'inc_nav.php'; ?>    
+<?php include 'inc_nav.php'; ?>
     <main class="contain">
       <h1>Kort om ABM-media as</h1>
        <p><strong>ABM-Media as</strong> er et forlag som gir ut fagbladene <a href="http://www.bokogbibliotek.no">Bok og Bibliotek</a> og <a href="http://www.museumsnytt.no/">Museumsnytt</a> samt <a href="http://www.tidsskriftetarkiv.no">Tidsskriftet Arkiv</a>.</p>
        <p>ABM-Media AS skal ha som intensjon &aring; etablere publiseringskanaler for arkiv, bibliotek og museumsomr&aring;dene som er godkjent i UH-sektorens finansieringssystem.</p>
        <p>Les mer <a href="om.php">om ABM-media</a>, hvordan du <a href="kontakt.php">kommer i kontakt</a> med oss, eller se p&aring; <a href="utgivelser.php">utgivelsene</a> v&aring;re som kan bestilles.</p>
         <h1>Nyheter</h1>
-        <h2>Noark 30 &aring;r &ndash; erfaringar, status og utsikter </h2>
-        <p>Tidsskriftet Arkiv inviterer til (Re)lanseringskonferanse den 1. desember kl. 10 &ndash; 15. </p>
-        <p>Sted: H&oslash;gskolen i Oslo og Akershus, Pilestredet 48, S141.</p>
-        <p><a href="http://www.abm-media.no/Relanseringskonferanse.pdf">Last ned invitasjon og p&aring;meldingsskjema [pdf]</a></p>
-        <p>Det er 30 &aring;r sidan den f&oslash;rste Noark-spesifikasjonen kom og vart startskotet til ei utvikling som
-sette norsk offentleg forvaltning i ei s&aelig;rstilling n&aring;r det gjeld elektronisk arkivdanning. P&aring;
-konferansen &oslash;nskjer vi &aring; setje s&oslash;kjelyset p&aring; viktige erfaringar fr&aring; utviklingsarbeidet, p&aring; Noark som
-verkt&oslash;y i dagens arkivdanning og n&aring;r det gjeld bevaringa av elektr
-oniske arkiv. Vi har lagt opp programmet med ein hovudinnleiar, ein kommentator og ein kort debatt under kvart tema, f&oslash;r vi
-avsluttar med ein paneldebatt der hovudinnleiarane deltar.</p>
-   <h3>Program</h3>
-   <table class="program">
-       <tr>
-          <th>10:00</th>
-          <td>
-           Utviklinga av Noark &ndash; utfordringar og l&aelig;rdommar<br/>
-           Hovudinnleiar: Trond Sirev&aring;g<br/>
-           Kommentator: Bj&oslash;rn Bering<br/>
-           </td>
-       </tr>
-       <tr>
-          <th>12:00</th>
-          <td>
-           Lunsjpause
-           </td>
-       </tr>
-       <tr>
-          <th>12:45</th>
-          <td>
-            Noark som verkt&oslash;y for arkivbevaring<br/>
-            Hovudinnleiar: Olav Satasl&aring;tten<br/>
-            Kommentator: Liv Heidi Siljebr&aring;ten<br/>
-           </td>
-       </tr>
-       <tr>
-          <th>13:00</th>
-          <td>
-           Paneldebatt
-           </td>
-       </tr>
-   </table>
-   
-   <p>Konferanseavgift kr. 200. Bindande p&aring;melding med faktureringsadresse innan 25. november til
-<a href="mailto:gudmund.valderhaug@hioa.no">gudmund.valderhaug@hioa.no</a></p>
+        <p>2015-09-30: Oppdatert prisliste og lagt til ny utgivelse: <a href="http://www.abm-media.no/utgivelser.php#9788293298090">Wilhelm Reich og Trygve Braatøy og Aadel Bülow-Hansen-tradisjonen i Norge. Utviklingen fra interessegruppe til akademisk utdanning i årene 1956-2006.</a></p>
 
-<p>Tidsskriftet Arkiv (<a href="http://www.tidsskriftetarkiv.no">www.tidsskriftetarkiv.no</a>) har som form&aring;l &aring; styrke den vitskaplege tiln&aelig;rminga
-til arkivfaget og vere ein uavhengig arena for fagleg utvikling og debatt. Tidsskriftet er nettbasert
-og blir publisert av ABM-media. I l&oslash;pet av november blir tidsskriftet relansert i ny bunad og p&aring;
-    ny plattform.</p>
-    </main>
-    
 <?php include 'inc_footer.php'; ?>
 <p class="visits">Antall bes&oslash;k:
 <script type="text/javascript" src="http://www.dittnettsted.com/teller/getcount.php?id=f4cea91095"></script>
---
+++ nyhetsarkiv.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Nyhetsarkiv – gammelt nytt | ABM-media AS</title>
+    <link rel="stylesheet" href="abm.css?v=4" type="text/css">
+    <style type="text/css">
+        table.program th {
+            vertical-align: top;
+        }
+        table.program {line-height: 1.75rem;}
+        table.program td {padding-left: 2rem;}
+		.published {font-style: italic;}
+    </style>
+</head>
+
+<body>
+<?php include 'inc_header.php'; ?>
+<?php include 'inc_nav.php'; ?>
+    <main class="contain">
+      <h1>Nyhetsarkiv</h1>
+       	<article>
+        <h2>Noark 30 &aring;r &ndash; erfaringar, status og utsikter </h2>
+        <p class="published">Publisert: november 2014</p>
+        <p>Tidsskriftet Arkiv inviterer til (Re)lanseringskonferanse den 1. desember 2014 kl. 10 &ndash; 15. </p>
+        <p>Sted: H&oslash;gskolen i Oslo og Akershus, Pilestredet 48, S141.</p>
+        <p><a href="http://www.abm-media.no/Relanseringskonferanse.pdf">Last ned invitasjon og p&aring;meldingsskjema [pdf]</a></p>
+        <p>Det er 30 &aring;r sidan den f&oslash;rste Noark-spesifikasjonen kom og vart startskotet til ei utvikling som
+sette norsk offentleg forvaltning i ei s&aelig;rstilling n&aring;r det gjeld elektronisk arkivdanning. P&aring;
+konferansen &oslash;nskjer vi &aring; setje s&oslash;kjelyset p&aring; viktige erfaringar fr&aring; utviklingsarbeidet, p&aring; Noark som
+verkt&oslash;y i dagens arkivdanning og n&aring;r det gjeld bevaringa av elektr
+oniske arkiv. Vi har lagt opp programmet med ein hovudinnleiar, ein kommentator og ein kort debatt under kvart tema, f&oslash;r vi
+avsluttar med ein paneldebatt der hovudinnleiarane deltar.</p>
+   <h3>Program</h3>
+   <table class="program">
+       <tr>
+          <th>10:00</th>
+          <td>
+           Utviklinga av Noark &ndash; utfordringar og l&aelig;rdommar<br/>
+           Hovudinnleiar: Trond Sirev&aring;g<br/>
+           Kommentator: Bj&oslash;rn Bering<br/>
+           </td>
+       </tr>
+       <tr>
+          <th>12:00</th>
+          <td>
+           Lunsjpause
+           </td>
+       </tr>
+       <tr>
+          <th>12:45</th>
+          <td>
+            Noark som verkt&oslash;y for arkivbevaring<br/>
+            Hovudinnleiar: Olav Satasl&aring;tten<br/>
+            Kommentator: Liv Heidi Siljebr&aring;ten<br/>
+           </td>
+       </tr>
+       <tr>
+          <th>13:00</th>
+          <td>
+           Paneldebatt
+           </td>
+       </tr>
+   </table>
+   
+   <p>Konferanseavgift kr. 200. Bindande p&aring;melding med faktureringsadresse innan 25. november til
+<a href="mailto:gudmund.valderhaug@hioa.no">gudmund.valderhaug@hioa.no</a></p>
+
+<p>Tidsskriftet Arkiv (<a href="http://www.tidsskriftetarkiv.no">www.tidsskriftetarkiv.no</a>) har som form&aring;l &aring; styrke den vitskaplege tiln&aelig;rminga
+til arkivfaget og vere ein uavhengig arena for fagleg utvikling og debatt. Tidsskriftet er nettbasert
+og blir publisert av ABM-media. I l&oslash;pet av november blir tidsskriftet relansert i ny bunad og p&aring;
+    ny plattform.</p></article>
+    </main>
+    
+<?php include 'inc_footer.php'; ?>
+<p class="visits">Antall bes&oslash;k:
+<script type="text/javascript" src="http://www.dittnettsted.com/teller/getcount.php?id=f4cea91095"></script>
+    </p>
+</body>
+
+</html>
