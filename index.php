d63618f9c5a9a9a209ef58e332a40d221f743605
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-03-10 13:04:14 +0100

    fjern nyheter

--- index.php
+++ index.php
@@ -22,9 +22,6 @@
        <p><strong>ABM-Media as</strong> er et forlag som gir ut fagbladene <a href="http://www.bokogbibliotek.no">Bok og Bibliotek</a> og <a href="http://www.museumsnytt.no/">Museumsnytt</a> samt <a href="http://www.tidsskriftetarkiv.no">Tidsskriftet Arkiv</a>.</p>
        <p>ABM-Media AS skal ha som intensjon &aring; etablere publiseringskanaler for arkiv, bibliotek og museumsomr&aring;dene som er godkjent i UH-sektorens finansieringssystem.</p>
        <p>Les mer <a href="om.php">om ABM-media</a>, hvordan du <a href="kontakt.php">kommer i kontakt</a> med oss, eller se p&aring; <a href="utgivelser.php">utgivelsene</a> v&aring;re som kan bestilles.</p>
-        <h1>Nyheter</h1>
-		<p>2015-09-30: Oppdatert prisliste</p>
-		<p>Siste publikasjon: <a href="http://www.abm-media.no/utgivelse.php?isbn=9788293298113">Norsk lærarutdanning i etterkrigstida. Ei utvikling i spenning mellom tradisjon og fornying.</a></p>
 
 <?php include 'inc_footer.php'; ?>
 <p class="visits">Antall bes&oslash;k:
